12602ce4f7d72fe0b3ff8d71b882326a6ae599c1
stone.cat <stone.cat.online@gmail.com>
2021-11-09 16:40:36 +0000

    structure additions

---
+++ common_components/atoms/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Wrapper } from './Button.style'
+
+const Button = () => {
+    return (<Wrapper>
+        button
+    </Wrapper>);
+}
+
+export default Button;
---
+++ common_components/atoms/Button/Button.style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.button`
+    height: 80px;
+    width: 120px;
+    background-color: lightcoral;
+`
